4522b046788fe876c8f9666d24862154a76eecc1
codeprismtechnologies <chandan.rai@codeprism.in>
2024-01-22 22:47:24 +0530

    test sonar qube pr analysis

--- src/main/java/com/example/testsonarqubepranalysis/TestSonarQubePrAnalysisApplication.java
+++ src/main/java/com/example/testsonarqubepranalysis/TestSonarQubePrAnalysisApplication.java
@@ -11,6 +11,7 @@ public class TestSonarQubePrAnalysisApplication {
         System.out.println("Hello World!");
         System.out.println("Hello World!");
         System.out.println("Hello World!");
+        System.out.println("Hello World!");
     }
 
 }
