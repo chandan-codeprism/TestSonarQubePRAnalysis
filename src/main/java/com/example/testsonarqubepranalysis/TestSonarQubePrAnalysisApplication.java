e6faf46c0ad48bd009516c4edc1e02a9a5cde6a5
codeprismtechnologies <chandan.rai@codeprism.in>
2024-01-22 16:42:22 +0530

    Initial commit

---
+++ src/main/java/com/example/testsonarqubepranalysis/TestSonarQubePrAnalysisApplication.java
@@ -0,0 +1,13 @@
+package com.example.testsonarqubepranalysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSonarQubePrAnalysisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestSonarQubePrAnalysisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testsonarqubepranalysis/TestSonarQubePrAnalysisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testsonarqubepranalysis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSonarQubePrAnalysisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
